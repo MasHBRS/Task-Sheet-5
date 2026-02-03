0d05833d6ac9253e05b771b0e7310036c4a8d33c
Aidin <AidinBnn378@gmail.com>
2026-02-04 00:43:48 +0100

    proj

--- assignment.py
+++ assignment.py
@@ -6,12 +6,13 @@ import math
 # -------------------- CONSTANTS --------------------
 WIDTH, HEIGHT = 1000, 500
 SIZE = (WIDTH, HEIGHT)
-CIRCLE_RADIUS=100
+Charging_RADIUS=150
 LIGHT_RADIUS=20
 CHARGING_POS=(0,0)
-BIRD_POS=(200,100)
-BIRD_ANGLE=0
-BIRD_SCALE=0.1
+TANK_POS=(400,250)
+TANK_ANGLE=90
+TANK_SCALE=0.1
+TANK_BATTERY_LEVEL=150
 GRAY = (150, 150, 150)
 GRAY_BORDER=(40,40,40)
 GREEN = (0, 200, 0)
@@ -21,22 +22,39 @@ BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 FPS = 60
 MAX_TRACE_DISTANCE=100
-SL= SM= SR= 0
+VL= VR= SL= SM= SR= 0
 Battery_Max=200
+
 def drawMap(screen):
     screen.fill(GRAY)
-    pygame.draw.circle(screen, BLACK, CHARGING_POS,CIRCLE_RADIUS)
+    pygame.draw.circle(screen, BLACK, CHARGING_POS,Charging_RADIUS)
     pygame.draw.circle(screen, YELLOW, CHARGING_POS,LIGHT_RADIUS)
 
-def loadBirdImage():
+def loadTANKImage():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(current_dir, 'tank.png')
-    birdImage = pygame.image.load(path).convert_alpha()
-    return birdImage
+    TANKImage = pygame.image.load(path).convert_alpha()
+    return TANKImage
 
-def trace_to_screen(start_pos, theta_deg, distance, screen_size):
+def trace_to_light(TANK_rect, theta_deg, distance, screen_size):
+    W, H = screen_size
+    diagon = math.hypot(W, H)
+
+    screen_rect = pygame.Rect(0, 0, W, H)
+
+    start_pos_x_front_sensor = int(TANK_rect.center[0]+(61//2)*math.cos(math.radians(TANK_ANGLE)))
+    start_pos_y_front_sensor = int(TANK_rect.center[1]-(61//2)*math.sin(math.radians(TANK_ANGLE)))
+    pygame.draw.circle(screen, (255, 255, 255), (start_pos_x_front_sensor, start_pos_y_front_sensor), 5)
+    
+    start_pos_x_back_sensor = int(TANK_rect.center[0]-(61//2)*math.cos(math.radians(TANK_ANGLE)))
+    start_pos_y_back_sensor = int(TANK_rect.center[1]+(61//2)*math.sin(math.radians(TANK_ANGLE)))
+    pygame.draw.circle(screen, (255, 255, 255), (start_pos_x_back_sensor, start_pos_y_back_sensor), 5)
+
+    return math.hypot(start_pos_x_front_sensor,start_pos_y_front_sensor)/diagon, math.hypot(start_pos_x_back_sensor,start_pos_y_back_sensor)/diagon
+
+def trace_to_screen(TANK_rect, theta_deg, distance, screen_size):
     """
-    start_pos: (x,y) start point (e.g., image_rect.center)
+    TANK_rect: rectangle of the tank image
     theta_deg: angle in degrees (0 -> right, 90 -> up)
     distance: ray length
     screen_size: (W,H)
@@ -47,12 +65,13 @@ def trace_to_screen(start_pos, theta_deg, distance, screen_size):
     W, H = screen_size
     screen_rect = pygame.Rect(0, 0, W, H)
 
-    theta = math.radians(theta_deg)
-
+    start_pos_x = int(TANK_rect.center[0]+(61//2)*math.cos(math.radians(TANK_ANGLE)))
+    start_pos_y = int(TANK_rect.center[1]-(61//2)*math.sin(math.radians(TANK_ANGLE)))
+    start_pos = (start_pos_x, start_pos_y)
     # Pygame screen coords: +x right, +y down, so "up" is -sin
     end = (
-        start_pos[0] + math.cos(theta) * distance,
-        start_pos[1] - math.sin(theta) * distance
+        start_pos[0] + math.cos(math.radians(theta_deg)) * distance,
+        start_pos[1] - math.sin(math.radians(theta_deg)) * distance
     )
 
     clipped = screen_rect.clipline(start_pos, end)  # () if no intersection/visible
@@ -71,46 +90,77 @@ def trace_to_screen(start_pos, theta_deg, distance, screen_size):
         return S
     return 
 
-def drawBird(screen, angle,birdImage,birdScale):
-    bird_drawn_surf = pygame.transform.rotozoom(birdImage, angle, birdScale)
-    bird_rect = bird_drawn_surf.get_rect(center=BIRD_POS)
-    screen.blit(bird_drawn_surf, bird_rect)
-    return bird_drawn_surf, bird_rect
+def ground_sensor_is_in_black_area(screen,TANK_rect):
+        x,y=TANK_rect.center[0],TANK_rect.center[1]
+        if math.sqrt(x**2+y**2)<=Charging_RADIUS:
+            return True
+        return False
+
+def drawTANK(screen, angle,TANKImage,TANKScale):
+    TANK_drawn_surf = pygame.transform.rotozoom(TANKImage, angle, TANKScale)
+    TANK_rect = TANK_drawn_surf.get_rect(center=TANK_POS)
+    screen.blit(TANK_drawn_surf, TANK_rect)
+    return TANK_drawn_surf, TANK_rect 
+
+def displayValues(screen, SL, SM, SR, font,charging_area,front,back,battery_level, VL, VR):
+    try:
+        text_SL = font.render(f"SL: {SL:.3f}" if SL !=(None, None) else 'SL: None', True, BLUE)
+        text_SM = font.render(f"SM: {SM:.3f}" if SM !=(None, None) else 'SM: None', True, BLUE)
+        text_SR = font.render(f"SR: {SR:.3f}" if SR !=(None, None) else 'SR: None', True, BLUE)
+        text_charging_area = font.render(f"Grnd: {1 if charging_area else 0}", True, BLUE)
+        text_front = font.render(f"Frnt: {front:.3f}", True, BLUE)
+        text_back = font.render(f"Bck: {back:.3f}", True, BLUE)
+        text_battery_level = font.render(f"Batt: {battery_level}", True, BLUE)
+        text_VL = font.render(f"VL: {VL:.3f}", True, BLUE)
+        text_VR = font.render(f"VR: {VR:.3f}", True, BLUE)
+    except:
+        battery_level=0
+    
+    
+    screen.blit(text_SL, (WIDTH*0.9, 10))
+    screen.blit(text_SM, (WIDTH*0.9, 30))
+    screen.blit(text_SR, (WIDTH*0.9, 50))
+    screen.blit(text_charging_area, (WIDTH*0.9, 70))
+    screen.blit(text_front, (WIDTH*0.9, 90))
+    screen.blit(text_back, (WIDTH*0.9, 110))
+    screen.blit(text_battery_level, (WIDTH*0.9, 130))
+    screen.blit(text_VL, (WIDTH*0.9, 150))
+    screen.blit(text_VR, (WIDTH*0.9, 170))
 
 pygame.init()
+font = pygame.font.SysFont(None, 30)
 screen = pygame.display.set_mode(SIZE)
 drawMap(screen)
 pygame.display.set_caption("Random Points + collidepoint (pygame 2.6.1)")
 clock = pygame.time.Clock()
-birdImage=loadBirdImage()
-bird_drawn_surf, bird_rect=drawBird(screen,BIRD_ANGLE,birdImage,BIRD_SCALE)
+TANKImage=loadTANKImage()
+TANK_drawn_surf, TANK_rect=drawTANK(screen,TANK_ANGLE,TANKImage,TANK_SCALE)
 
-start = bird_rect.center          # image center
-SR = trace_to_screen(start, BIRD_ANGLE, MAX_TRACE_DISTANCE, screen.get_size())
-SM = trace_to_screen(start, BIRD_ANGLE+90, MAX_TRACE_DISTANCE, screen.get_size())
-SL = trace_to_screen(start, BIRD_ANGLE+180, MAX_TRACE_DISTANCE, screen.get_size())
-
-# -------------------- RECT + POINTS --------------------
-
+index=0
 running = True
+x=0
+y=0
+TANK_drawn_surf, TANK_rect=drawTANK(screen,TANK_ANGLE,TANKImage,TANK_SCALE)
+
 while running:
+    x+=1
+    y+=1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     drawMap(screen)
-    bird_drawn_surf, bird_rect=drawBird(screen,BIRD_ANGLE,birdImage,BIRD_SCALE)
-
-    start = bird_rect.center          # image center
-    SR = trace_to_screen(start, BIRD_ANGLE, MAX_TRACE_DISTANCE, screen.get_size())
-    SM = trace_to_screen(start, BIRD_ANGLE+90, MAX_TRACE_DISTANCE, screen.get_size())
-    SL = trace_to_screen(start, BIRD_ANGLE+180, MAX_TRACE_DISTANCE, screen.get_size())
-    print(f"{SR=}, {SM=}, {SL=}")
     
+    SR = trace_to_screen(TANK_rect, TANK_ANGLE-90, MAX_TRACE_DISTANCE, screen.get_size())
+    SM = trace_to_screen(TANK_rect, TANK_ANGLE, MAX_TRACE_DISTANCE, screen.get_size())
+    SL = trace_to_screen(TANK_rect, TANK_ANGLE+90, MAX_TRACE_DISTANCE, screen.get_size())
+    front,back=trace_to_light(TANK_rect, TANK_ANGLE, MAX_TRACE_DISTANCE, screen.get_size())
+    charging_area=ground_sensor_is_in_black_area(screen,TANK_rect)
+    TANK_rect.move_ip([-1,-1])
+    screen.blit(TANK_drawn_surf, TANK_rect)
 
-    # -------------------- RECT + POINTS --------------------
-    rect = pygame.Rect(220, 200, 250, 140)
+    displayValues(screen, SL, SM, SR, font,charging_area,front,back, TANK_BATTERY_LEVEL, VL, VR)
+    
     pygame.display.flip()
-    clock.tick(FPS)
-
+    clock.tick(FPS) 
 pygame.quit()
