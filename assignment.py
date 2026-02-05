cdeb5b12cb5232469a0d33d463a7de92f06a9047
Aidin <AidinBnn378@gmail.com>
2026-02-05 08:36:10 +0100

    not really good.

--- assignment.py
+++ assignment.py
@@ -102,6 +102,21 @@ def drawTANK(screen, angle,TANKImage,TANKScale):
     screen.blit(TANK_drawn_surf, TANK_rect)
     return TANK_drawn_surf, TANK_rect 
 
+#TANK_drawn_surf, TANK_rect
+def moveTank(TANK_rect,speed,Tank_angle,wheel_base=2,dt=1):#assume speed=[vl,vr]
+    #TANK_rect.move_ip(speed)
+    v = (speed[0] + speed[1]) / 2                 # forward speed px/s
+    omega = (speed[1] - speed[0]) / wheel_base  # turn rate rad/s
+    dx =math.cos(Tank_angle)  if Tank_angle%3==0 else 1 #v * math.cos(Tank_angle)*dt
+    dy =math.sin(Tank_angle)  if Tank_angle%3==0 else 1#v * math.sin(Tank_angle)*dt
+    TANK_rect.move_ip(dx, dy)
+
+    
+    Tank_angle += 3  if Tank_angle%3==0 else 1  #math.degrees(Tank_angle) - 90   # rotozoom: 0°=up
+    TANK_drawn_surf = pygame.transform.rotozoom(TANKImage, Tank_angle, TANK_SCALE)
+    TANK_drawn_surf.get_rect(center=TANK_rect.center)
+    return TANK_drawn_surf, TANK_rect,Tank_angle
+
 def displayValues(screen, SL, SM, SR, font,charging_area,front,back,battery_level, VL, VR):
     try:
         text_SL = font.render(f"SL: {SL:.3f}" if SL !=(None, None) else 'SL: None', True, BLUE)
@@ -138,13 +153,11 @@ TANK_drawn_surf, TANK_rect=drawTANK(screen,TANK_ANGLE,TANKImage,TANK_SCALE)
 
 index=0
 running = True
-x=0
-y=0
+speed=[1,1]
 TANK_drawn_surf, TANK_rect=drawTANK(screen,TANK_ANGLE,TANKImage,TANK_SCALE)
 
 while running:
-    x+=1
-    y+=1
+    dt = clock.tick(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -156,7 +169,7 @@ while running:
     SL = trace_to_screen(TANK_rect, TANK_ANGLE+90, MAX_TRACE_DISTANCE, screen.get_size())
     front,back=trace_to_light(TANK_rect, TANK_ANGLE, MAX_TRACE_DISTANCE, screen.get_size())
     charging_area=ground_sensor_is_in_black_area(screen,TANK_rect)
-    TANK_rect.move_ip([-1,-1])
+    TANK_drawn_surf, TANK_rect,TANK_ANGLE=moveTank(TANK_rect,speed,TANK_ANGLE,wheel_base=500,dt=dt)
     screen.blit(TANK_drawn_surf, TANK_rect)
 
     displayValues(screen, SL, SM, SR, font,charging_area,front,back, TANK_BATTERY_LEVEL, VL, VR)
