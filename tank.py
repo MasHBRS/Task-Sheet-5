193781368fc572c5c55fd092546964b24ddd3915
Aidin <AidinBnn378@gmail.com>
2026-02-05 23:42:12 +0100

    adding tank.py

--- tank.py
+++ tank.py
@@ -1,7 +1,9 @@
-class tank():
-    def __init__(self,screen=None,position=(200,100),angle=0,scale=0.1):
-        self.position=position
-        self.angle=angle
-        self.scale=scale
+class Tank():
+    def __init__(self, individual_weight):
+        self.individual_weight = individual_weight
+        self.fitness_value = 0
 
-    def update(self,screen,birdImage):
+    def  step(sellf):
+        pass
+
+    
